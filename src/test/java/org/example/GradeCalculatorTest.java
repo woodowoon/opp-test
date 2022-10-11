f05c1d1fed8d7f166358b9e5764851f97e330f5b
dowoon <woodw920@gmail.com>
2022-10-11 14:01:28 +0900

    객체지향적인 학점관리 opp test

--- src/test/java/org/example/GradeCalculatorTest.java
+++ src/test/java/org/example/GradeCalculatorTest.java
@@ -1,14 +1,36 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
- *  평균학점 계산 방법 = (학점수×교과목 평점)의 합계/수강신청 총학점 수
+ *  평균학점 계산 방법 = (학점수×교과목 평점)의 합계 / 수강신청 총학점 수
  *  MVC패턴(Model-View-Controller) 기반으로 구현한다.
  *  일급 컬렉션 사용
  */
 public class GradeCalculatorTest {
     // 학점계산기 도메인 : 이수한 과목(객체지향프로그래밍, 자료구조, 중국어회화), 학점 계산기
-    // 이수한 과목 : 객체지향프로그래밍, 자료구조, 중국어회화 --> 과목(코스) 클래스
+    // 객체지향프로그래밍, 자료구조, 중국어회화 --> 과목(코스) 클래스
 
-    // 이수한 과목을 전달하여 평균학점 계산 요청 ---> 학점 계산기
+    /**
+     * 핵심 포인트
+     */
+    // 이수한 과목을 전달하여 평균학점 계산 요청 ---> 학점 계산기 ---> (학점수 X 교과목 평점)의 합계  ---> 과목(코스)
+    //                                                    ---> 수강신청 총 학점 수           ---> 과목(코스)
 
+    @DisplayName("평균 학점을 계산한다.")
+    @Test
+    void calculateGradeTest() {
+        List<Course> courses = List.of(new Course("OOP", 3, "A+"),
+                new Course("자료구조", 3, "A+"));
+
+        GradeCalculator gradeCalculator = new GradeCalculator(courses);
+        double gradeResult = gradeCalculator.calculateGrade();
+
+        assertThat(gradeResult).isEqualTo(4.5);
+    }
 }
