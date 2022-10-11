50f374ecfdf2ac760cad2c3c9d69a10916dcd0b2
dowoon <woodw920@gmail.com>
2022-10-11 15:56:15 +0900

    Cook 요리 TDD 개발

---
+++ src/test/java/org/example/cook/MemuItemTest.java
@@ -0,0 +1,16 @@
+package org.example.cook;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class MemuItemTest {
+
+    @DisplayName("메뉴 항목을 생성한다.")
+    @Test
+    void createTest() {
+        assertThatCode(() -> new MenuItem("만두", 5000))
+                .doesNotThrowAnyException();
+    }
+}
--- src/test/java/org/example/customer/CustomerTest.java
+++
@@ -1,5 +0,0 @@
-package org.example.customer;
-
-public class CustomerTest {
-
-}
